5a17411e83918fbb9a135feef72d5f9d1e2181e8
haodong0518 <99232125+haodong0518@users.noreply.github.com>
2024-03-09 19:37:10 -0600

    Add files via upload

--- hw4/main.py
+++ hw4/main.py
@@ -13,13 +13,13 @@ import build_evolution
 import motion
 import mutate   
 
-evo_times =20
+evo_times =5
 basic_num =10
 # ***Step 1. Initialize 5 robots, with 3 legs with different locations. 
 # Create a dict "Survive", key is the file name, value is the fitness score
 survive = dict()
 num_species = 10
-basic_module_num= 1
+basic_module_num= 2
 for i in range(num_species):
     mj,chassis = build_evolution.build_env()
     build_evolution.single_evolution(chassis, basic_module_num)
@@ -38,20 +38,22 @@ for i in range(evo_times):
     # Step 2.1:  Delete the lowest robos:
     
     keys_to_delete = sorted(survive, key=survive.get)[:5]
-    print(survive)
     print(f"keys_to_delete {keys_to_delete}")
-    for key in keys_to_delete:
-        os.remove(key)
-        del survive[key]
-    #print(f"deleted, length {len(survive)} {survive}")
-    
-    # Step 2.2: Mutate the rest robos and keep the survived robos:
-    sub_dict = dict()
-    for robo in survive:
-        key = mutate.mutate(survive, robo)
-        #print(key)
-        sub_dict[key] = motion.fitness_cal(key,0)
-    survive = mutate.melt_dictionaries(survive, sub_dict)
+    try:
+        for key in keys_to_delete:
+            os.remove(key)
+            del survive[key]
+        print(f"deleted, length {len(survive)} {survive}")
+        
+        # Step 2.2: Mutate the rest robos and keep the survived robos:
+        sub_dict = dict()
+        for robo in survive:
+            key = mutate.mutate(survive, robo)
+            #print(key)
+            sub_dict[key] = motion.fitness_cal(key,0)
+        survive = mutate.melt_dictionaries(survive, sub_dict)
+    except:
+        pass
 
-# for robo in survive:
-#     motion.fitness_cal(robo, 1)
+for robo in survive:
+    motion.fitness_cal(robo, 1)
